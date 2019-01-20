9de1993c8e4d8858c41243dc9b328377d702af2f
Pelle Braendgaard <pelle@stakeventures.com>
2019-01-20 11:38:56 -0600

    export toEthereumAddress

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import SimpleSigner from './SimpleSigner'
 import NaclSigner from './NaclSigner'
+import { toEthereumAddress } from './Digest'
 import { verifyJWT, createJWT, decodeJWT } from './JWT'
 
-module.exports = { SimpleSigner, NaclSigner, verifyJWT, createJWT, decodeJWT }
+module.exports = { SimpleSigner, NaclSigner, verifyJWT, createJWT, decodeJWT, toEthereumAddress }
