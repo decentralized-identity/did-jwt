8da6ab3df450fbececa26b470e9c499b3b0be91c
Mike Xu <mxu11290@gmail.com>
2019-05-03 10:19:48 -0400

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,11 @@ var path = require('path')
 
 module.exports = {
   entry: './lib/index.js',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ]
-  },
   output: {
-    filename: 'uport-jwt.js',
+    filename: 'did-jwt.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
+    library: 'DID-JWT'
   }
 }
