e951196898ed33f132b428e0932d68d8425f7907
Joel Torstensson <oed3933@gmail.com>
2019-05-16 11:55:48 +0200

    feat: Expose Verifier- and SignerAlgorithm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import SimpleSigner from './SimpleSigner'
 import NaclSigner from './NaclSigner'
 import { verifyJWT, createJWT, decodeJWT } from './JWT'
+import VerifierAlgorithm from './VerifierAlgorithm'
+import SignerAlgorithm from './SignerAlgorithm'
 
-module.exports = { SimpleSigner, NaclSigner, verifyJWT, createJWT, decodeJWT }
+module.exports = { SimpleSigner, NaclSigner, verifyJWT, createJWT, decodeJWT, SignerAlgorithm, VerifierAlgorithm }
