174b4d656f24e431af6d63604d0a239b348c3f9a
Mike Xu <mxu11290@gmail.com>
2019-08-09 17:16:41 -0400

    refactor: change createJWT to set iat by default instead of nbf

--- src/__tests__/JWT-test.ts
+++ src/__tests__/JWT-test.ts
@@ -107,7 +107,7 @@ describe('createJWT()', () => {
 
     it('creates a JWT with expiry in 10000 seconds', () => {
       return createJWT(
-        { requested: ['name', 'phone'] },
+        { requested: ['name', 'phone'], nbf: Math.floor(new Date().getTime()/1000) },
         { issuer: did, signer, expiresIn: 10000 }
       ).then(jwt => {
         const { payload } = decodeJWT(jwt)
@@ -152,7 +152,7 @@ describe('createJWT()', () => {
 
     it('creates a JWT with expiry in 10000 seconds', () => {
       return createJWT(
-        { requested: ['name', 'phone'] },
+        { requested: ['name', 'phone'], nbf: Math.floor(new Date().getTime()/1000) },
         { alg, issuer: did, signer, expiresIn: 10000 }
       ).then(jwt => {
         const { payload } = decodeJWT(jwt)
