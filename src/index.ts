897b2a5d501ec9d5cf047a947cd5f66a56ec3339
Joel Torstensson <oed3933@gmail.com>
2020-04-27 10:39:18 +0200

    fix: export JWS functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import SimpleSigner from './SimpleSigner'
 import NaclSigner from './NaclSigner'
-import { verifyJWT, createJWT, decodeJWT, Signer } from './JWT'
+import { verifyJWT, createJWT, decodeJWT, verifyJWS, createJWS, Signer } from './JWT'
 import { toEthereumAddress } from './Digest'
 
-export { SimpleSigner, NaclSigner, verifyJWT, createJWT, decodeJWT, toEthereumAddress, Signer }
+export { SimpleSigner, NaclSigner, verifyJWT, createJWT, decodeJWT, verifyJWS, createJWS, toEthereumAddress, Signer }
