72e72933551706ea7acc5cbe12bb352b365de4f5
Joel Torstensson <oed3933@gmail.com>
2020-04-28 11:41:39 +0200

    fix: make SimpleSigner return a string

--- src/SignerAlgorithm.ts
+++ src/SignerAlgorithm.ts
@@ -1,32 +1,27 @@
 import base64url from 'uport-base64url'
 import { Buffer } from 'buffer'
 import { Signer, EcdsaSignature, SignerAlgorithm } from './JWT'
+import { ecdsaSigToJose } from './SimpleSigner'
 
 function instanceOfEcdsaSignature(object: any): object is EcdsaSignature {
   return typeof object === 'object' && 'r' in object && 's' in object
 }
 
 export function ES256KSigner(recoverable?: boolean): SignerAlgorithm {
-  function toJose({ r, s, recoveryParam }: EcdsaSignature): string {
-    const jose: Buffer = Buffer.alloc(recoverable ? 65 : 64)
-    Buffer.from(r, 'hex').copy(jose, 0)
-    Buffer.from(s, 'hex').copy(jose, 32)
-    if (recoverable) {
-      if (recoveryParam === undefined) {
-        throw new Error('Signer did not return a recoveryParam')
-      }
-      jose[64] = recoveryParam
-    }
-    return base64url.encode(jose)
-  }
-
   return async function sign(payload: string, signer: Signer): Promise<string> {
-    const signature: EcdsaSignature | string = await signer(payload)
+    let signature: EcdsaSignature | string = await signer(payload)
     if (instanceOfEcdsaSignature(signature)) {
-      return toJose(signature)
-    } else {
-      throw new Error('expected a signer function that returns a signature object instead of string')
+      console.warn('A deprecated version of SimpleSigner detected. Please make sure to update')
+      signature = ecdsaSigToJose(signature)
     }
+    const sigBuf = base64url.toBuffer(signature)
+    if (recoverable && sigBuf.length !== 65) {
+      throw new Error('Signer did not return a recoveryParam')
+    } else if (!recoverable) {
+      // Remove recoveryParam if present
+      if (sigBuf.length === 65) signature = base64url.encode(sigBuf.slice(0, 64))
+    }
+    return signature
   }
 }
 
--- src/SimpleSigner.ts
+++ src/SimpleSigner.ts
@@ -1,6 +1,7 @@
 import { ec as EC, ec } from 'elliptic'
+import base64url from 'uport-base64url'
 import { sha256 } from './Digest'
-import { Signer } from './JWT'
+import { Signer, EcdsaSignature } from './JWT'
 
 const secp256k1: EC = new EC('secp256k1')
 
@@ -8,6 +9,15 @@ function leftpad(data: string, size = 64): string {
   if (data.length === size) return data
   return '0'.repeat(size - data.length) + data
 }
+
+export function ecdsaSigToJose({ r, s, recoveryParam }: EcdsaSignature): string {
+  const jose: Buffer = Buffer.alloc(65)
+  Buffer.from(r, 'hex').copy(jose, 0)
+  Buffer.from(s, 'hex').copy(jose, 32)
+  jose[64] = recoveryParam
+  return base64url.encode(jose)
+}
+
 /**
  *  The SimpleSigner returns a configured function for signing data. It also defines
  *  an interface that you can also implement yourself and use in our other modules.
@@ -26,11 +36,12 @@ function SimpleSigner(hexPrivateKey: string): Signer {
   const privateKey: ec.KeyPair = secp256k1.keyFromPrivate(hexPrivateKey)
   return async data => {
     const { r, s, recoveryParam }: EC.Signature = privateKey.sign(sha256(data))
-    return {
+    const signatureObj: EcdsaSignature = {
       r: leftpad(r.toString('hex')),
       s: leftpad(s.toString('hex')),
       recoveryParam
     }
+    return ecdsaSigToJose(signatureObj)
   }
 }
 
