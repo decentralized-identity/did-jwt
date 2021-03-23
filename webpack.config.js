6d448def81f2977a09993b820ecffce622330a4e
Mircea Nistor <mirceanis@gmail.com>
2021-03-23 20:48:07 +0100

    fix(deps): bump webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ var path = require('path')
 
 module.exports = {
   entry: './lib/index.js',
+  mode: 'production',
+  resolve: {
+    fallback: {
+      crypto: false,
+      // crypto: require.resolve('crypto-browserify'),
+      util: false,
+      // util: require.resolve('util/'),
+    }
+  },
   output: {
     filename: 'did-jwt.js',
     path: path.resolve(__dirname, 'dist'),
