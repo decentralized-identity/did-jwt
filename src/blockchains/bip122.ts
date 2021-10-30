8d531822325af5f337b54986790ae81c2e3b04c1
daoauth <57783762+daoauth@users.noreply.github.com>
2021-10-30 14:55:03 +0900

    remove ripemd160

--- src/blockchains/bip122.ts
+++ src/blockchains/bip122.ts
@@ -1,10 +1,10 @@
-import RIPEMD160 from 'ripemd160'
 import { bytesToBase58 } from '../util'
 import { sha256 } from '../Digest'
+import { Ripemd160 } from './utils/ripemd160'
 
 export const publicKeyToAddress = (publicKey: string): string => {
   const publicKeyBuffer = Uint8Array.from(Buffer.from(publicKey, 'hex'))
-  const hash = new RIPEMD160().update(Buffer.from(sha256(publicKeyBuffer))).digest()
+  const hash = new Ripemd160().update(Buffer.from(sha256(publicKeyBuffer))).digest()
   const step1 = Buffer.concat([Buffer.from('00', 'hex'), hash])
   const checksum = Buffer.from(sha256(sha256(step1)))
     .toString('hex')
--- src/blockchains/cosmos.ts
+++ src/blockchains/cosmos.ts
@@ -1,10 +1,13 @@
-import RIPEMD160 from 'ripemd160'
-import { sha256 } from '../Digest'
+import { ec as EC } from 'elliptic'
 import { bech32 } from 'bech32'
+import { sha256 } from '../Digest'
+import { Ripemd160 } from './utils/ripemd160'
 
 export const publicKeyToAddress = (publicKey: string, prefix: string): string => {
-  const publicKeyBuffer = Uint8Array.from(Buffer.from(publicKey, 'hex'))
-  const hash = new RIPEMD160().update(Buffer.from(sha256(publicKeyBuffer))).digest()
+  const ec = new EC('secp256k1')
+  const compressedPublicKey = ec.keyFromPublic(publicKey, 'hex').getPublic().encode('hex', true)
+  const publicKeyBuffer = Uint8Array.from(Buffer.from(compressedPublicKey, 'hex'))
+  const hash = new Ripemd160().update(Buffer.from(sha256(publicKeyBuffer))).digest()
   const words = bech32.toWords(hash)
   return bech32.encode(prefix, words).replace(prefix, '')
 }
