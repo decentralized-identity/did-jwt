831e0f0865c4acb0b4dbdfd168d5452dee0ce5ea
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-03-03 18:52:22 -0600

    add EcdsaSignature interface to src/SignerAlg/common_SignerAlg.ts

--- src/SignerAlg/common_SignerAlg.ts
+++ src/SignerAlg/common_SignerAlg.ts
@@ -1,3 +1,5 @@
+import { EcdsaSignature } from '../util'
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function instanceOfEcdsaSignature(object: any): object is EcdsaSignature {
   return typeof object === 'object' && 'r' in object && 's' in object
