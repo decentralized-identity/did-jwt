5cf650bd35029953f3a1c9ee3bfb5e521f90770b
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-03-04 14:59:42 -0600

    add recoverySigner and signature result for test in ES256Signer in SigneAlg

--- src/__tests__/SignerAlg/ES256SignerAlg.test.ts
+++ src/__tests__/SignerAlg/ES256SignerAlg.test.ts
@@ -8,6 +8,7 @@ const secp256r1 = new EC('p256')
 const privateKey = '736f625c9dda78a94bb16840c82779bb7bc18014b8ede52f0f03429902fc4ba8'
 const kp = secp256r1.keyFromPrivate(privateKey)
 const signer = ES256Signer(privateKey)
+const recoverySigner = ES256Signer(privateKey, true)
 
 describe('SignerAlgorithm', () => {
   it('supports ES256', () => {
@@ -57,32 +58,30 @@ describe('ES256-R', () => {
  
   expect.assertions(1)
   it('returns correct signature', async () => {
-    return await expect(jwtSigner('hello', signer)).resolves.toEqual(
-      'Zks0QO1ma5pHHtNbpb0qDap0VJSvQvA775N0GZsAp3PQjmDGbsfyKlUVcU9PFueIXksioSTsPXiOCgAHIOe4WA'
+    return await expect(jwtSigner('hello', recoverySigner)).resolves.toEqual(
+      'Zks0QO1ma5pHHtNbpb0qDap0VJSvQvA775N0GZsAp3PQjmDGbsfyKlUVcU9PFueIXksioSTsPXiOCgAHIOe4WAE'
     )
   })
-  /* 
+   
   it('returns signature of 64 bytes', async () => {
     expect.assertions(1)
-    const signature = await jwtSigner('hello', signer)
+    const signature = await jwtSigner('hello', recoverySigner)
     expect(base64ToBytes(signature).length).toEqual(65)
   })
   
   it('contains r, s and recoveryParam of signature', async () => {
     expect.assertions(1)
-    const signature = await jwtSigner('hello', signer)
+    const signature = await jwtSigner('hello', recoverySigner)
     expect(toSignatureObject(signature, true)).toEqual({
-      r: '31a08f708ca94bbe939ef2926e16cf306d35049be343aa2e21357e995b7bfdb7',
-      s: 'd365f1a411dc28a12a9bccf0470256571b3618beb4e71f833d945dce64c76945',
+      r: '664b3440ed666b9a471ed35ba5bd2a0daa745494af42f03bef9374199b00a773',
+      s: 'd08e60c66ec7f22a5515714f4f16e7885e4b22a124ec3d788e0a000720e7b858',
       recoveryParam: 1,
     })
   })
-  */
-  /*  
+   
   it('can verify the signature', async () => {
     expect.assertions(1)
-    const signature = await jwtSigner('hello', signer)
+    const signature = await jwtSigner('hello', recoverySigner)
     expect(kp.verify(sha256('hello'), toSignatureObject(signature, true))).toBeTruthy()
   })
-  */
 })
