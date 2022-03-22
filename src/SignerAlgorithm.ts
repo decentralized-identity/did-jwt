260e21491207c31424e93e42614bec7864a76a78
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-03-22 12:06:07 -0500

    change imports for Verifier and Signer Algorithms

--- src/SignerAlgorithm.ts
+++ src/SignerAlgorithm.ts
@@ -1,25 +1,25 @@
 import { SignerAlgorithm } from './JWT'
 
-import * as ES256KSignerAlg from './SignerAlg/ES256KSignerAlg'
-import * as Ed25519SignerAlg from './SignerAlg/Ed25519SignerAlg'
+import { ES256KSignerAlg } from './SignerAlg/ES256KSignerAlg'
+import { Ed25519SignerAlg } from './SignerAlg/Ed25519SignerAlg'
 
-import * as ES256SignerAlg from './SignerAlg/ES256SignerAlg'
+import { ES256SignerAlg } from './SignerAlg/ES256SignerAlg'
 
 interface SignerAlgorithms {
   [alg: string]: SignerAlgorithm
 }
 
 const algorithms: SignerAlgorithms = {
-  ES256: ES256SignerAlg.ES256SignerAlg(),
-  'ES256-R': ES256SignerAlg.ES256SignerAlg(true),
-  ES256K: ES256KSignerAlg.ES256KSignerAlg(),
+ ES256: ES256SignerAlg(),
+ 'ES256-R': ES256SignerAlg(true),
+ ES256K: ES256KSignerAlg(),
   // This is a non-standard algorithm but retained for backwards compatibility
   // see https://github.com/decentralized-identity/did-jwt/issues/146
-  'ES256K-R': ES256KSignerAlg.ES256KSignerAlg(true),
+  'ES256K-R': ES256KSignerAlg(true),
   // This is actually incorrect but retained for backwards compatibility
   // see https://github.com/decentralized-identity/did-jwt/issues/130
-  Ed25519: Ed25519SignerAlg.Ed25519SignerAlg(),
-  EdDSA: Ed25519SignerAlg.Ed25519SignerAlg(),
+  Ed25519: Ed25519SignerAlg(),
+  EdDSA: Ed25519SignerAlg(),
 }
 
 function SignerAlg(alg: string): SignerAlgorithm {
--- src/VerifierAlgorithm.ts
+++ src/VerifierAlgorithm.ts
@@ -1,10 +1,13 @@
 import type { VerificationMethod } from 'did-resolver'
 import { base64ToBytes, bytesToHex, EcdsaSignature } from './util'
 
-import * as Ed25519VerifierAlg from './VerifierAlg/Ed25519VerifierAlg'
-import * as ES256KVerifierAlg from './VerifierAlg/ES256KVerifierAlg'
+import { verifyEd25519 } from './VerifierAlg/Ed25519VerifierAlg'
 
-import * as ES256VerifierAlg from './VerifierAlg/ES256VerifierAlg'
+import { verifyES256K } from './VerifierAlg/ES256KVerifierAlg'
+import { verifyRecoverableES256K } from './VerifierAlg/ES256KVerifierAlg'
+
+import { verifyES256 } from './VerifierAlg/ES256VerifierAlg'
+import { verifyRecoverableES256 } from './VerifierAlg/ES256VerifierAlg'
 
 // converts a JOSE signature to it's components
 export function toSignatureObject(signature: string, recoverable = false): EcdsaSignature {
@@ -26,18 +29,16 @@ interface Algorithms {
   [name: string]: Verifier
 }
 const algorithms: Algorithms = {
-  ES256: ES256VerifierAlg.verifyES256,
-  'ES256-R': ES256VerifierAlg.verifyRecoverableES256,
-  ES256K: ES256KVerifierAlg.verifyES256K,
+  ES256: verifyES256,
+  'ES256-R': verifyRecoverableES256,
+  ES256K: verifyES256K,
   // This is a non-standard algorithm but retained for backwards compatibility
   // see https://github.com/decentralized-identity/did-jwt/issues/146
-  'ES256K-R': ES256KVerifierAlg.verifyRecoverableES256K,
+  'ES256K-R': verifyRecoverableES256K,
   // This is actually incorrect but retained for backwards compatibility
   // see https://github.com/decentralized-identity/did-jwt/issues/130
-  // Ed25519: verifyEd25519,
-  Ed25519: Ed25519VerifierAlg.verifyEd25519,
-  // EdDSA: verifyEd25519,
-  EdDSA: Ed25519VerifierAlg.verifyEd25519,
+  Ed25519: verifyEd25519,
+  EdDSA: verifyEd25519,
 }
 
 function VerifierAlgorithm(alg: string): Verifier {
