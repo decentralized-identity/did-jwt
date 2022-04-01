f39c88e8742ff6cc25509df9e1a1f2b288a7a739
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-03-31 23:18:47 -0500

    update util.ts with latest matcher

--- src/util.ts
+++ src/util.ts
@@ -85,7 +85,6 @@ export function toSealed(ciphertext: string, tag: string): Uint8Array {
 }
 
 const hexMatcher = /^(0x)?([a-fA-F0-9]{64}|[a-fA-F0-9]{128})$/
-// const base58Matcher = /^([1-9A-HJ-NP-Za-km-z]{44}|[1-9A-HJ-NP-Za-km-z]{88})$/
 const base58Matcher = /^([1-9A-HJ-NP-Za-km-z]{43,44}|[1-9A-HJ-NP-Za-km-z]{86,88})$/
 const base64Matcher = /^([0-9a-zA-Z=\-_+/]{43}|[0-9a-zA-Z=\-_+/]{86})(={0,2})$/
 
