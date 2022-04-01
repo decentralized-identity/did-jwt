027f70189bdd175c11212bb3c96dfab8ef718e2c
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-03-31 23:15:02 -0500

    remove encrypters and clean up to remote forks state

--- src/encrypters/ecdhEsA256Kw.ts
+++
@@ -1,2 +0,0 @@
-// See section 4.1 of rfc7518: https://www.rfc-editor.org/rfc/rfc7518.html (accessed: March 10, 2022)
-// See https://github.com/w3c-ccg/lds-jws2020/#supported-jose-algorithms (accessed: March 10, 2022)
