b4fc0e26e4afc6816735510974de6bc9347cdfcd
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-04-01 17:00:36 -0500

    remove commented out code with ES256KSigner.test.ts

--- src/__tests__/ES256KSigner.test.ts
+++ src/__tests__/ES256KSigner.test.ts
@@ -6,7 +6,6 @@ describe('Secp256k1 Signer', () => {
     expect.assertions(1)
     const privateKey = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
     const signer = ES256KSigner(hexToBytes(privateKey))
-    // const signer = ES256KSigner(privateKey)
     const plaintext = 'thequickbrownfoxjumpedoverthelazyprogrammer'
     await expect(signer(plaintext)).resolves.toEqual(
       'jsvdLwqr-O206hkegoq6pbo7LJjCaflEKHCvfohBP9XJ4C7mG2TPL9YjyKEpYSXqqkUrfRoCxQecHR11Uh7POw'
@@ -17,7 +16,6 @@ describe('Secp256k1 Signer', () => {
     expect.assertions(1)
     const privateKey = '0x278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
     const signer = ES256KSigner(hexToBytes(privateKey))
-    // const signer = ES256KSigner(privateKey)
     const plaintext = 'thequickbrownfoxjumpedoverthelazyprogrammer'
     await expect(signer(plaintext)).resolves.toEqual(
       'jsvdLwqr-O206hkegoq6pbo7LJjCaflEKHCvfohBP9XJ4C7mG2TPL9YjyKEpYSXqqkUrfRoCxQecHR11Uh7POw'
@@ -28,7 +26,6 @@ describe('Secp256k1 Signer', () => {
     expect.assertions(1)
     const privateKey = '3fMGokRKc5yGVqbCXyGNTrp3vP1cXs86tsVSVwzhNvXQ'
     const signer = ES256KSigner(base58ToBytes(privateKey))
-    //const signer = ES256KSigner(privateKey)
     const plaintext = 'thequickbrownfoxjumpedoverthelazyprogrammer'
     await expect(signer(plaintext)).resolves.toEqual(
       'jsvdLwqr-O206hkegoq6pbo7LJjCaflEKHCvfohBP9XJ4C7mG2TPL9YjyKEpYSXqqkUrfRoCxQecHR11Uh7POw'
@@ -39,7 +36,6 @@ describe('Secp256k1 Signer', () => {
     expect.assertions(1)
     const privateKey = 'J4pd5wDin6ro5A42bsUBK17GPTbsd-iiQXFUzB0lOD8'
     const signer = ES256KSigner(base64ToBytes(privateKey))
-    // const signer = ES256KSigner(privateKey)
     const plaintext = 'thequickbrownfoxjumpedoverthelazyprogrammer'
     await expect(signer(plaintext)).resolves.toEqual(
       'jsvdLwqr-O206hkegoq6pbo7LJjCaflEKHCvfohBP9XJ4C7mG2TPL9YjyKEpYSXqqkUrfRoCxQecHR11Uh7POw'
@@ -50,7 +46,6 @@ describe('Secp256k1 Signer', () => {
     expect.assertions(1)
     const privateKey = 'J4pd5wDin6ro5A42bsUBK17GPTbsd+iiQXFUzB0lOD8='
     const signer = ES256KSigner(base64ToBytes(privateKey))
-    // const signer = ES256KSigner(privateKey)
     const plaintext = 'thequickbrownfoxjumpedoverthelazyprogrammer'
     await expect(signer(plaintext)).resolves.toEqual(
       'jsvdLwqr-O206hkegoq6pbo7LJjCaflEKHCvfohBP9XJ4C7mG2TPL9YjyKEpYSXqqkUrfRoCxQecHR11Uh7POw'
@@ -61,7 +56,6 @@ describe('Secp256k1 Signer', () => {
     expect.assertions(1)
     const privateKey = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d2538'
     expect(() => {
-     // ES256KSigner(privateKey)
       ES256KSigner(hexToBytes(privateKey))
     }).toThrowError(/^bad_key: Invalid private key format.*/)
   })
@@ -71,7 +65,6 @@ describe('Secp256k1 Signer', () => {
     const privateKey =
       '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
     expect(() => {
-     // ES256KSigner(privateKey)
       ES256KSigner(hexToBytes(privateKey))
     }).toThrowError(/^bad_key: Invalid private key format.*/)
   })
