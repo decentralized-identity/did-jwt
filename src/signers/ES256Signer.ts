169e7bdf23f91a8dbfb49b2ff8688c08e34017cd
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-09-01 19:40:01 -0500

    removed recoverable parameter from ES256Signer since only recoverable with secp256k1 not secp256r1

--- src/signers/ES256Signer.ts
+++ src/signers/ES256Signer.ts
@@ -7,7 +7,7 @@ import elliptic from 'elliptic'
 const secp256r1 = new elliptic.ec('p256')
 
 /**
- *  Creates a configured signer function for signing data using the ES256K (secp256r1 + sha256) algorithm.
+ *  Creates a configured signer function for signing data using the ES256 (secp256r1 + sha256) algorithm.
  *
  *  The signing function itself takes the data as a `Uint8Array` or `string` and returns a `base64Url`-encoded signature
  *
@@ -18,10 +18,9 @@ const secp256r1 = new elliptic.ec('p256')
  *  ```
  *
  *  @param    {String}    privateKey   a private key as `Uint8Array`
- *  @param    {Boolean}   recoverable  an optional flag to add the recovery param to the generated signatures
  *  @return   {Function}               a configured signer function `(data: string | Uint8Array): Promise<string>`
  */
-export function ES256Signer(privateKey: Uint8Array, recoverable = false): Signer {
+export function ES256Signer(privateKey: Uint8Array): Signer {
   const privateKeyBytes: Uint8Array = privateKey
   if (privateKeyBytes.length !== 32) {
     throw new Error(`bad_key: Invalid private key format. Expecting 32 bytes, but got ${privateKeyBytes.length}`)
@@ -29,14 +28,10 @@ export function ES256Signer(privateKey: Uint8Array, recoverable = false): Signer
   const keyPair: elliptic.ec.KeyPair = secp256r1.keyFromPrivate(privateKeyBytes)
 
   return async (data: string | Uint8Array): Promise<string> => {
-    const { r, s, recoveryParam }: elliptic.ec.Signature = keyPair.sign(sha256(data))
-    return toJose(
-      {
-        r: leftpad(r.toString('hex')),
-        s: leftpad(s.toString('hex')),
-        recoveryParam,
-      },
-      recoverable
-    )
+    const { r, s }: elliptic.ec.Signature = keyPair.sign(sha256(data))
+    return toJose({
+      r: leftpad(r.toString('hex')),
+      s: leftpad(s.toString('hex')),
+    })
   }
 }
