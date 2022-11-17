e78f3880856cc3aeca4d7dc8fa6a418158d30d33
Jack Tanner <jack@jackandtheblockstalk.com>
2022-11-17 22:40:53 +0100

    abstracted the conditionalWeightedThreshold verification

--- src/ConditionalAlgorithm.ts
+++ src/ConditionalAlgorithm.ts
@@ -7,17 +7,17 @@ export type Signer = (data: string | Uint8Array) => Promise<EcdsaSignature | str
 export type SignerAlgorithm = (payload: string, signer: Signer) => Promise<string>
 
 // TODO return VerificationMethod???
-export async function verifyConditionalProof(jwt: string, authenticator: VerificationMethod): Promise<VerificationMethod> {
+export async function verifyConditionalProof(
+  jwt: string,
+  authenticator: VerificationMethod
+): Promise<VerificationMethod> {
   // validate that nested signatures are valid so that we know that each level is indirectly signing the VC
   // TODO JWT has already been deceded, so better use the already decoded version
   let decoded = decodeJWT(jwt, false)
 
-  const threshold = authenticator.threshold as number
-  let signaturesThresholdCount = 0
-
   let jwtNestedLevelCount = 1
 
-  const signers: string[] = [] // string of DID URLs to the verification method or submethod
+  let signers: string[] = [] // string of DID URLs to the verification method or submethod
 
   let conditionSatisfied = false
 
@@ -26,41 +26,21 @@ export async function verifyConditionalProof(jwt: string, authenticator: Verific
   do {
     console.log(`verifyConitionalProof(): checking JWT at level ${jwtNestedLevelCount}`)
     const { header, data, payload, signature } = decoded
-    let validSignatureFound = false
+    let newSigners: string[] = []
 
     // Iterate through the condition
     if (authenticator.conditionWeightedThreshold) {
-      for (const condition of authenticator.conditionWeightedThreshold) {
-        // TODO this should call verifyJWT() instead recursively
-        let foundSigner: VerificationMethod | undefined
-        try {
-          console.log(`testing to see if ${condition.condition.id} matches`)
-          foundSigner = await verifyJWSDecoded({ header, data, signature } as JWSDecoded, condition.condition)
-        } catch (e) {
-          if (!((e as Error).message.startsWith('invalid_signature:'))) throw e
-        }
-
-        if (foundSigner && !signers.includes(foundSigner.id)) {
-          console.log(`verifyConditionalProof(): signature valid and is unique for ${foundSigner.id}`)
-          signers.push(foundSigner.id)
-          signaturesThresholdCount += condition.weight
-          validSignatureFound = true
-
-          console.log(
-            `verifyConditionalProof(): signaturesThresholdCount ${signaturesThresholdCount} >= threshold ${threshold}`
-          )
-          if (signaturesThresholdCount >= threshold) {
-            console.log(`verifyConditionalProof(): condition valid: ${authenticator.id}`)
-            // NOTE: we still need to go through each (nested) JWT level and check there is a valid signature
-            // so we don't `return true` here
-            conditionSatisfied = true
-          }
-        }
-      }
+      ({ conditionSatisfied, newSigners, signers } = await verifyConditionWeightedThreshold(
+        { header, data, signature } as JWSDecoded,
+        authenticator,
+        signers
+      ))
+    } else if (authenticator.conditionDelegated) {
+      // TODO
     }
 
     // Each (nested) JWT must be signed by at least one valid signature from the issuer all the way to the bottom
-    if (!validSignatureFound) {
+    if (newSigners.length === 0) {
       throw new Error(
         `${JWT_ERROR.RESOLVER_ERROR}: Invalid signature at nested level ${jwtNestedLevelCount} with signer ${signature}`
       )
@@ -79,9 +59,59 @@ export async function verifyConditionalProof(jwt: string, authenticator: Verific
 
   if (!conditionSatisfied) {
     throw new Error(
-      `${JWT_ERROR.INVALID_SIGNATURE}: JWT not valid. issuer DID document does not contain a verificationMethod that matches the signature.`
+      `${JWT_ERROR.INVALID_SIGNATURE}: JWT not valid. issuer ${authenticator.id} does not have a verificationMethod that matches the signature.`
     )
   }
 
   return authenticator
+}
+
+type VerifyConditionResponse = {
+  signers: string[]
+  newSigners: string[]
+  conditionSatisfied: boolean
+}
+
+async function verifyConditionWeightedThreshold(
+  { header, data, signature }: JWSDecoded,
+  authenticator: VerificationMethod,
+  signers: string[]
+): Promise<VerifyConditionResponse> {
+  if (!authenticator.conditionWeightedThreshold || !authenticator.threshold) {
+    throw new Error('Expected conditionWeightedThreshold and threshold')
+  }
+
+  const newSigners: string[] = []
+  let conditionSatisfied = false
+
+  const threshold = authenticator.threshold
+  let signaturesThresholdCount = 0
+
+  for (const condition of authenticator.conditionWeightedThreshold) {
+    let foundSigner: VerificationMethod | undefined
+
+    try {
+      console.log(`testing to see if ${condition.condition.id} matches`)
+      // TODO this should probably call verifyJWT() instead recursively
+      foundSigner = await verifyJWSDecoded({ header, data, signature } as JWSDecoded, condition.condition)
+    } catch (e) {
+      if (!((e as Error).message.startsWith('invalid_signature:'))) throw e
+    }
+
+    if (foundSigner && !signers.includes(foundSigner.id)) {
+      console.log(`verifyConditionalProof(): signature valid and is unique for ${foundSigner.id}`)
+      signers.push(foundSigner.id)
+      signaturesThresholdCount += condition.weight
+      newSigners.push(foundSigner.id)
+
+      console.log(
+        `verifyConditionalProof(): signaturesThresholdCount ${signaturesThresholdCount} >= threshold ${threshold}`
+      )
+      if (signaturesThresholdCount >= threshold) {
+        console.log(`verifyConditionalProof(): condition valid: ${authenticator.id}`)
+        conditionSatisfied = true
+      }
+    }
+  }
+  return { signers, newSigners, conditionSatisfied }
 }
