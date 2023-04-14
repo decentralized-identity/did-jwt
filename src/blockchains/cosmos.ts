93763ad6d70a599d48122e841076038c0eee5051
Sergey Ukustov <sergey@ukstv.me>
2023-04-14 20:58:48 +0300

    chore: Less const functions

--- src/blockchains/cosmos.ts
+++ src/blockchains/cosmos.ts
@@ -2,7 +2,7 @@ import { secp256k1 } from '@noble/curves/secp256k1'
 import { bech32 } from 'bech32'
 import { sha256, ripemd160 } from '../Digest'
 
-export const publicKeyToAddress = (publicKey: string, prefix: string): string => {
+export function publicKeyToAddress(publicKey: string, prefix: string): string {
   const publicKeyBuffer = secp256k1.ProjectivePoint.fromHex(publicKey).toRawBytes()
   const hash = ripemd160(sha256(publicKeyBuffer))
   const words = bech32.toWords(hash)
