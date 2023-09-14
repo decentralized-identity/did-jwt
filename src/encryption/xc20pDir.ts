a467902979e026f1b8b9493eb0bc78adeedc7433
Paul Miller <paul@paulmillr.com>
2023-09-14 22:08:55 +0000

    chacha: replace old lib with noble

--- src/encryption/xc20pDir.ts
+++ src/encryption/xc20pDir.ts
@@ -1,17 +1,17 @@
 import type { Decrypter, Encrypter, EncryptionResult, ProtectedHeader } from './types.js'
 import { bytesToBase64url, encodeBase64url } from '../util.js'
 import { fromString } from 'uint8arrays/from-string'
-import { XChaCha20Poly1305 } from '@stablelib/xchacha20poly1305'
+import { xchacha20poly1305 } from '@noble/ciphers/chacha';
 import { randomBytes } from '@noble/hashes/utils'
 
 export function xc20pEncrypter(key: Uint8Array): (cleartext: Uint8Array, aad?: Uint8Array) => EncryptionResult {
-  const cipher = new XChaCha20Poly1305(key)
   return (cleartext: Uint8Array, aad?: Uint8Array) => {
-    const iv = randomBytes(cipher.nonceLength)
-    const sealed = cipher.seal(iv, cleartext, aad)
+    const iv = randomBytes(24);
+    const cipher = xchacha20poly1305(key, iv, aad)
+    const sealed = cipher.encrypt(cleartext)
     return {
-      ciphertext: sealed.subarray(0, sealed.length - cipher.tagLength),
-      tag: sealed.subarray(sealed.length - cipher.tagLength),
+      ciphertext: sealed.subarray(0, sealed.length - 16),
+      tag: sealed.subarray(sealed.length - 16),
       iv,
     }
   }
@@ -39,10 +39,12 @@ export function xc20pDirEncrypter(key: Uint8Array): Encrypter {
 }
 
 export function xc20pDirDecrypter(key: Uint8Array): Decrypter {
-  const cipher = new XChaCha20Poly1305(key)
-
   async function decrypt(sealed: Uint8Array, iv: Uint8Array, aad?: Uint8Array): Promise<Uint8Array | null> {
-    return cipher.open(iv, sealed, aad)
+    try {
+      return xchacha20poly1305(key, iv, aad).decrypt(sealed)
+    } catch (error) {
+      return null;
+    }
   }
 
   return { alg: 'dir', enc: 'XC20P', decrypt }
