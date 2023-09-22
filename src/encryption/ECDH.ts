3385ea0e9e985e65386f3d095ab1b25fdcdad9b9
Brent Shambaugh <brent.shambaugh@gmail.com>
2023-09-22 14:39:44 -0500

    added preliminary updates to ECDH.ts for P256 to iniate discussion on a PR

--- src/encryption/ECDH.ts
+++ src/encryption/ECDH.ts
@@ -1,4 +1,5 @@
 import { x25519 } from '@noble/curves/ed25519'
+import { p256 } from '@noble/curves/p256'
 import type { ECDH } from './types.js'
 
 /**
@@ -17,5 +18,24 @@ export function createX25519ECDH(mySecretKey: Uint8Array): ECDH {
       throw new Error('invalid_argument: incorrect publicKey key length for X25519')
     }
     return x25519.getSharedSecret(mySecretKey, theirPublicKey)
+  }
+}
+
+/**
+ * Wraps an P256 secret key into an ECDH method that can be used to compute a shared secret with a public key.
+ * @param mySecretKey A `Uint8Array` of length 32 representing the bytes of my secret key
+ * @returns an `ECDH` method with the signature `(theirPublicKey: Uint8Array) => Promise<Uint8Array>`
+ *
+ * @throws 'invalid_argument:...' if the secret key size is wrong
+ */
+export function createP256ECDH(mySecretKey: Uint8Array): ECDH {
+  if (mySecretKey.length !== 32) {
+    throw new Error('invalid_argument: incorrect secret key length for P256')
+  }
+  return async (theirPublicKey: Uint8Array): Promise<Uint8Array> => {
+    if (theirPublicKey.length !== 33) {
+      throw new Error('invalid_argument: incorrect publicKey key length for P256')
+    }
+    return p256.getSharedSecret(mySecretKey, theirPublicKey)
   }
 }
