db07b81b3dbbe1e8df7dd6d37b700902fc5da1f4
Mircea Nistor <mirceanis@gmail.com>
2023-09-27 20:37:37 +0200

    docs: explain the different key lengths for EdDSASigner

--- src/signers/EdDSASigner.ts
+++ src/signers/EdDSASigner.ts
@@ -5,7 +5,13 @@ import { bytesToBase64url, stringToBytes } from '../util.js'
 /**
  *  Creates a configured signer function for signing data using the EdDSA (Ed25519) algorithm.
  *
- *  The signing function itself takes the data as a `Uint8Array` or `string` and returns a `base64Url`-encoded signature
+ *  The private key is expected to be a `Uint8Array` of 32 bytes, but for compatibility 64 bytes are also acceptable.
+ * Users of `@stablelib/ed25519` or `tweetnacl` will be able to use the 64 byte secret keys that library generates.
+ * These libraries precompute the public key and append it as the last 32 bytes of the secretKey, to speed up later
+ * signing operations.
+ *
+ *  The signing function itself takes the data as a `Uint8Array` or utf8 `string` and returns a `base64Url`-encoded
+ * signature
  *
  *  @example
  *  ```typescript
@@ -13,7 +19,7 @@ import { bytesToBase64url, stringToBytes } from '../util.js'
  *  const signature: string = await sign(data)
  *  ```
  *
- *  @param    {String}    secretKey   a 64 byte secret key as `Uint8Array`
+ *  @param    {String}    secretKey   a 32 or 64 byte secret key as `Uint8Array`
  *  @return   {Function}              a configured signer function `(data: string | Uint8Array): Promise<string>`
  */
 export function EdDSASigner(secretKey: Uint8Array): Signer {
