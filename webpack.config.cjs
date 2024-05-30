ea2fe58d10a23bee65761e95717d0f780829eb68
Mark Krasner <mzkrasner@gmail.com>
2024-05-30 13:43:19 -0400

    Adding in building of types and reverting change to unused webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,7 +3,7 @@ const path = require('path')
 // @type {import('webpack').Configuration}
 module.exports = {
   // entry: './lib/index.js',
-  entry: './dist/index.module.js',
+  entry: './lib/index.module.js',
   mode: 'production',
   resolve: {
     fallback: {
